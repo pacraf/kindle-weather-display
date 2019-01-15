840b5d28d82b0779a4321cc864f389c345096df5
pacraf <8949690+pacraf@users.noreply.github.com>
2019-01-15 13:43:21 +0100

    Update weather-script.sh

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -5,4 +5,5 @@ cd "$(dirname "$0")"
 python2 weather-script.py
 rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
 pngcrush -c 0 -ow weather-script-output.png
-cp -f weather-script-output.png /path/to/web/server/directory/weather-script-output.png
+
+cp -f weather-script-output.png /var/www/openmediavault/weather_out.png
