9ecece173f60c9d0a333dc5526ffdacfc72ee5bb
pacraf <8949690+pacraf@users.noreply.github.com>
2019-01-15 13:30:24 +0100

    Update weather-script.py

--- server/weather-script.py
+++ server/weather-script.py
@@ -1,68 +1,60 @@
-#!/usr/bin/python2
+# encoding=utf8
+import sys
 
-# Kindle Weather Display
-# Matthew Petroff (http://mpetroff.net/)
-# September 2012
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-from xml.dom import minidom
-import datetime
 import codecs
-try:
-    # Python 3
-    from urllib.request import urlopen
-except ImportError:
-    # Python 2
-    from urllib2 import urlopen
-
-#
-# Geographic location
-#
-
-latitude = 39.3286
-longitude = -76.6169
+import datetime
+from pyowm import OWM , timeutils
 
 
+API_key = 'xxxxxxxxxxxxxxxxxxxxx'
+owm = OWM(API_key)
+#obs = owm.weather_at_id(xxxxxx)
+#w=obs.get_weather()
 
-#
-# Download and parse weather data
-#
+fc = owm.three_hours_forecast_at_id(xxxxxx)
 
-# Fetch data (change lat and lon to desired location)
-weather_xml = urlopen('http://graphical.weather.gov/xml/SOAP_server/ndfdSOAPclientByDay.php?whichClient=NDFDgenByDay&lat=' + str(latitude) + '&lon=' + str(longitude) + '&format=24+hourly&numDays=4&Unit=e').read()
-dom = minidom.parseString(weather_xml)
+f=fc.get_forecast()
+f.get_reception_time('iso')
+f.get_location()
 
-# Parse temperatures
-xml_temperatures = dom.getElementsByTagName('temperature')
-highs = [None]*4
-lows = [None]*4
-for item in xml_temperatures:
-    if item.getAttribute('type') == 'maximum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            highs[i] = int(values[i].firstChild.nodeValue)
-    if item.getAttribute('type') == 'minimum':
-        values = item.getElementsByTagName('value')
-        for i in range(len(values)):
-            lows[i] = int(values[i].firstChild.nodeValue)
+lst =f.get_weathers()
 
-# Parse icons
-xml_icons = dom.getElementsByTagName('icon-link')
+for weather in f:
+    print(weather.get_reference_time('iso'),weather.get_status(), weather.get_temperature('celsius'))
+
+today = timeutils.now()
+today_12 = today.replace(hour=15)
+today_22 = today.replace(hour=22)
+day_two_12 = timeutils.tomorrow(12,00)
+day_two_22 = timeutils.tomorrow(22,00)
+day_three_12 = timeutils._timedelta_days(2).replace(hour=12)
+day_three_22 = timeutils._timedelta_days(2).replace(hour=22)
+day_four_12 = timeutils._timedelta_days(3).replace(hour=12)
+day_four_22 = timeutils._timedelta_days(3).replace(hour=22)
+
 icons = [None]*4
-for i in range(len(xml_icons)):
-    icons[i] = xml_icons[i].firstChild.nodeValue.split('/')[-1].split('.')[0].rstrip('0123456789')
-
-# Parse dates
-xml_day_one = dom.getElementsByTagName('start-valid-time')[0].firstChild.nodeValue[0:10]
-day_one = datetime.datetime.strptime(xml_day_one, '%Y-%m-%d')
+icons = [fc.get_weather_at(today_12).get_weather_icon_name(),fc.get_weather_at(day_two_12).get_weather_icon_name(),fc.get_weather_at(day_three_12).get_weather_icon_name(),fc.get_weather_at(day_four_12).get_weather_icon_name()]
 
 
+highs = [None]*4
+highs = [fc.get_weather_at(today_12).get_temperature('celsius').get('temp_max'),fc.get_weather_at(day_two_12).get_temperature('celsius').get('temp_max'),fc.get_weather_at(day_three_12).get_temperature('celsius').get('temp_max'),fc.get_weather_at(day_four_12).get_temperature('celsius').get('temp_max')]
+for i in range(len(highs)):
+    highs[i]= round(highs[i],1)
+
+lows = [None]*4
+lows = [fc.get_weather_at(today_22).get_temperature('celsius').get('temp_min'),fc.get_weather_at(day_two_22).get_temperature('celsius').get('temp_min'),fc.get_weather_at(day_three_22).get_temperature('celsius').get('temp_min'),fc.get_weather_at(day_four_22).get_temperature('celsius').get('temp_min')]
+for i in range(len(lows)):
+    lows[i]= round(lows[i],1)
 
 #
 # Preprocess SVG
 #
 
 # Open SVG to process
-output = codecs.open('weather-script-preprocess.svg', 'r', encoding='utf-8').read()
+output = codecs.open('owm-weather-script-preprocess.svg', 'r', encoding='utf-8').read()
 
 # Insert icons and temperatures
 output = output.replace('ICON_ONE',icons[0]).replace('ICON_TWO',icons[1]).replace('ICON_THREE',icons[2]).replace('ICON_FOUR',icons[3])
@@ -71,8 +63,8 @@ output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).
 
 # Insert days of week
 one_day = datetime.timedelta(days=1)
-days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-output = output.replace('DAY_THREE',days_of_week[(day_one + 2*one_day).weekday()]).replace('DAY_FOUR',days_of_week[(day_one + 3*one_day).weekday()])
+days_of_week = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątunio', 'Sobcia', 'Niedzielka']
+output = output.replace('DAY_ONE',days_of_week[today_12.weekday()]).replace('DAY_TWO',days_of_week[day_two_12.weekday()]).replace('DAY_THREE',days_of_week[day_three_12.weekday()]).replace('DAY_FOUR',days_of_week[day_four_12.weekday()])
 
 # Write output
 codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
