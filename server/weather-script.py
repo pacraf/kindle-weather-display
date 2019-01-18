c63749b0555cf0e731033341aac6842dbad0172b
pacraf <8949690+pacraf@users.noreply.github.com>
2019-01-18 13:36:40 +0100

    Update weather-script.py

--- server/weather-script.py
+++ server/weather-script.py
@@ -3,18 +3,17 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-
 import codecs
 import datetime
 from pyowm import OWM , timeutils
 
 
-API_key = 'xxxxxxxxxxxxxxxxxxxxx'
+API_key = 'e50fce3e6e5761cd48860978982f3a11'
 owm = OWM(API_key)
-#obs = owm.weather_at_id(xxxxxx)
+#obs = owm.weather_at_id(7532706)
 #w=obs.get_weather()
 
-fc = owm.three_hours_forecast_at_id(xxxxxx)
+fc = owm.three_hours_forecast_at_id(7532706)
 
 f=fc.get_forecast()
 f.get_reception_time('iso')
@@ -22,12 +21,23 @@ f.get_location()
 
 lst =f.get_weathers()
 
-for weather in f:
-    print(weather.get_reference_time('iso'),weather.get_status(), weather.get_temperature('celsius'))
+# for weather in f:
+#     print(weather.get_reference_time('iso'),weather.get_status(), weather.get_temperature('celsius'))
 
 today = timeutils.now()
-today_12 = today.replace(hour=15)
-today_22 = today.replace(hour=22)
+#today_12 = today.replace(hour=12)
+#today_12 = today
+if fc.when_starts('date').hour < 10 :
+    today_12 = today.replace(hour=12)
+else:
+    today_12 = today.replace(hour=fc.when_starts('date').hour)
+
+#today_22 = today.replace(hour=22)
+if fc.when_starts('date').hour < 20 :
+    today_22 = today.replace(hour=22)
+else:
+    today_22 = today.replace(hour=fc.when_starts('date').hour)
+
 day_two_12 = timeutils.tomorrow(12,00)
 day_two_22 = timeutils.tomorrow(22,00)
 day_three_12 = timeutils._timedelta_days(2).replace(hour=12)
@@ -38,16 +48,24 @@ day_four_22 = timeutils._timedelta_days(3).replace(hour=22)
 icons = [None]*4
 icons = [fc.get_weather_at(today_12).get_weather_icon_name(),fc.get_weather_at(day_two_12).get_weather_icon_name(),fc.get_weather_at(day_three_12).get_weather_icon_name(),fc.get_weather_at(day_four_12).get_weather_icon_name()]
 
+icons_text = [None]*4
+icons_text = [fc.get_weather_at(today_12).get_status(),fc.get_weather_at(day_two_12).get_status(),fc.get_weather_at(day_three_12).get_status(),fc.get_weather_at(day_four_12).get_status()]
+
 
 highs = [None]*4
 highs = [fc.get_weather_at(today_12).get_temperature('celsius').get('temp_max'),fc.get_weather_at(day_two_12).get_temperature('celsius').get('temp_max'),fc.get_weather_at(day_three_12).get_temperature('celsius').get('temp_max'),fc.get_weather_at(day_four_12).get_temperature('celsius').get('temp_max')]
 for i in range(len(highs)):
-    highs[i]= round(highs[i],1)
+    highs[i]= int(round(highs[i],0))
 
 lows = [None]*4
 lows = [fc.get_weather_at(today_22).get_temperature('celsius').get('temp_min'),fc.get_weather_at(day_two_22).get_temperature('celsius').get('temp_min'),fc.get_weather_at(day_three_22).get_temperature('celsius').get('temp_min'),fc.get_weather_at(day_four_22).get_temperature('celsius').get('temp_min')]
 for i in range(len(lows)):
-    lows[i]= round(lows[i],1)
+    lows[i]= int(round(lows[i],0))
+print fc.when_starts('date')
+print (icons_text)
+print (icons)
+print (highs)
+print (lows)
 
 #
 # Preprocess SVG
@@ -63,8 +81,9 @@ output = output.replace('LOW_ONE',str(lows[0])).replace('LOW_TWO',str(lows[1])).
 
 # Insert days of week
 one_day = datetime.timedelta(days=1)
-days_of_week = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątunio', 'Sobcia', 'Niedzielka']
-output = output.replace('DAY_ONE',days_of_week[today_12.weekday()]).replace('DAY_TWO',days_of_week[day_two_12.weekday()]).replace('DAY_THREE',days_of_week[day_three_12.weekday()]).replace('DAY_FOUR',days_of_week[day_four_12.weekday()])
+days_of_week = ['PoniedziaĹ‚ek', 'Wtorek', 'Ĺšroda', 'Czwartek', 'PiÄ…tunio', 'Sobcia', 'Niedzielka']
+output = output.replace('DAY_ONE',days_of_week[today_22.weekday()] + '   ' + timeutils.now().strftime("%d/%m/%y")).replace('DAY_TWO',days_of_week[day_two_12.weekday()]).replace('DAY_THREE',days_of_week[day_three_12.weekday()]).replace('DAY_FOUR',days_of_week[day_four_12.weekday()])
 
 # Write output
 codecs.open('weather-script-output.svg', 'w', encoding='utf-8').write(output)
+
